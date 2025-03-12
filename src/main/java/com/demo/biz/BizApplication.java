c1acca44560a7d35a927e54f1a790b591f741589
tutq <tranquangut.hust54@gmail.com>
2025-03-12 22:00:15 +0700

    update

--- src/main/java/com/demo/biz/BizApplication.java
+++ src/main/java/com/demo/biz/BizApplication.java
@@ -3,10 +3,12 @@ package com.demo.biz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EntityScan(basePackages = { "com.base", "com.demo" })
 @EnableJpaRepositories(basePackages = { "com.base", "com.demo" })
 @ComponentScan(basePackages = { "com.base", "com.demo" })
