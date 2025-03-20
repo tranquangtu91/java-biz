fd4a96f6499be364319d4b8e773725c30e294a59
tutq <tranquangtu.hust54@gmail.com>
2025-03-20 12:08:01 +0700

    update

--- src/main/java/com/base/admin/domain/entity/userrole/UserRole.java
+++ src/main/java/com/base/admin/domain/entity/userrole/UserRole.java
@@ -1,16 +1,16 @@
 package com.base.admin.domain.entity.userrole;
 
+import com.base.admin.domain.entity.role.Role;
+import com.base.admin.domain.entity.user.User;
+import com.base.common.domain.entity.SignedBaseEntity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-import com.base.admin.domain.entity.role.Role;
-import com.base.admin.domain.entity.user.User;
-import com.base.common.domain.entity.BaseEntity;
-
 @Entity
 @Table(name = "tbl_user_role")
-class UserRole extends BaseEntity {
+class UserRole extends SignedBaseEntity {
     @ManyToOne
     User user;
     @ManyToOne
