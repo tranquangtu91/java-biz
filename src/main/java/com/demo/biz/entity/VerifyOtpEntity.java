21fcb726ae4c7182347dd07707a963636dda94eb
anhvt61 <anhvt61@fpt.com>
2025-04-01 10:22:36 +0700

    init otp service in biz1

--- src/main/java/com/demo/biz/entity/VerifyOtpEntity.java
+++ src/main/java/com/demo/biz/entity/VerifyOtpEntity.java
@@ -5,15 +5,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class VerifyOtpEntity {
     private static Map<String, Boolean> userOtpStatus = new ConcurrentHashMap<>();
-
     public static boolean isPassVerifyOtp(String key) {
         return userOtpStatus.getOrDefault(key, false);
     }
-
     public static void setPassVerifyOtp(String key, boolean value) {
         userOtpStatus.put(key, value);
     }
-
     public static void clearPassVerifyOtp(String key) {
         userOtpStatus.remove(key);
     }
